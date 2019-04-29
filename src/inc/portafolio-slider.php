ef5adbdd6e5d7e67669ddb423d04fd448b13c0d5
Manu Chavarin <manuelchs@icloud.com>
2019-04-29 03:06:39 -0500

    ajustes landing

--- src/inc/portafolio-slider.php
+++ src/inc/portafolio-slider.php
@@ -11,61 +11,6 @@
                 <img src="#/img/arrow-right.png" alt="">
             </div>
             <div id="slider" class="slider">
-                <div class="slider-item" location="<?php echo $cazagol; ?>">
-                    <div class="item-content">
-                        <img class="item-bg" src="#/img/portafolio/cazagol-bg.png" alt="">
-                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
-                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
-                        <img class="logo" src="#/img/portafolio/cazagol-logo.png" alt="">
-                        <div class="description">
-                            <p>Branding<span> / </span>Web</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="slider-item" location="<?php echo $beauty; ?>">
-                    <div class="item-content">
-                        <img class="item-bg" src="#/img/portafolio/beauty-bg.png" alt="">
-                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
-                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
-                        <img class="logo" src="#/img/portafolio/beauty-logo.png" alt="">
-                        <div class="description">
-                            <p>Branding<span> / </span>Web</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="slider-item" location="<?php echo $chill; ?>">
-                    <div class="item-content">
-                        <img class="item-bg" src="#/img/portafolio/chill-bg.png" alt="">
-                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
-                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
-                        <img class="logo" src="#/img/portafolio/chill-logo.png" alt="">
-                        <div class="description">
-                            <p>Branding<span> / </span>Impresos</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="slider-item" location="<?php echo $dr; ?>">
-                    <div class="item-content">
-                        <img class="item-bg" src="#/img/portafolio/dr-bg.png" alt="">
-                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
-                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
-                        <img class="logo" src="#/img/portafolio/dr-logo.png" alt="">
-                        <div class="description">
-                            <p>Branding<span> / </span>Web<span> / </span>Impresos</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="slider-item" location="<?php echo $fit; ?>">
-                    <div class="item-content">
-                        <img class="item-bg" src="#/img/portafolio/fit-bg.png" alt="">
-                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
-                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
-                        <img class="logo" src="#/img/portafolio/fit-logo.png" alt="">
-                        <div class="description">
-                            <p>Branding<span> / </span>Web<span> / </span>Impresos</p>
-                        </div>
-                    </div>
-                </div>
                 <div class="slider-item" location="<?php echo $git; ?>">
                     <div class="item-content">
                         <img class="item-bg" src="#/img/portafolio/git-bg.png" alt="">
@@ -88,12 +33,12 @@
                         </div>
                     </div>
                 </div>
-                <div class="slider-item" location="<?php echo $intermed; ?>">
+                <div class="slider-item" location="<?php echo $fit; ?>">
                     <div class="item-content">
-                        <img class="item-bg" src="#/img/portafolio/intermed-bg.png" alt="">
+                        <img class="item-bg" src="#/img/portafolio/fit-bg.png" alt="">
                         <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
                         <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
-                        <img class="logo" src="#/img/portafolio/intermed-logo.png" alt="">
+                        <img class="logo" src="#/img/portafolio/fit-logo.png" alt="">
                         <div class="description">
                             <p>Branding<span> / </span>Web<span> / </span>Impresos</p>
                         </div>
@@ -121,6 +66,62 @@
                         </div>
                     </div>
                 </div>
+                <div class="slider-item" location="<?php echo $dr; ?>">
+                    <div class="item-content">
+                        <img class="item-bg" src="#/img/portafolio/dr-bg.png" alt="">
+                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
+                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
+                        <img class="logo" src="#/img/portafolio/dr-logo.png" alt="">
+                        <div class="description">
+                            <p>Branding<span> / </span>Web<span> / </span>Impresos</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-item" location="<?php echo $beauty; ?>">
+                    <div class="item-content">
+                        <img class="item-bg" src="#/img/portafolio/beauty-bg.png" alt="">
+                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
+                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
+                        <img class="logo" src="#/img/portafolio/beauty-logo.png" alt="">
+                        <div class="description">
+                            <p>Branding<span> / </span>Web</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-item" location="<?php echo $cazagol; ?>">
+                    <div class="item-content">
+                        <img class="item-bg" src="#/img/portafolio/cazagol-bg.png" alt="">
+                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
+                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
+                        <img class="logo" src="#/img/portafolio/cazagol-logo.png" alt="">
+                        <div class="description">
+                            <p>Branding<span> / </span>Web</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-item" location="<?php echo $chill; ?>">
+                    <div class="item-content">
+                        <img class="item-bg" src="#/img/portafolio/chill-bg.png" alt="">
+                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
+                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
+                        <img class="logo" src="#/img/portafolio/chill-logo.png" alt="">
+                        <div class="description">
+                            <p>Branding<span> / </span>Impresos</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-item" location="<?php echo $intermed; ?>">
+                    <div class="item-content">
+                        <img class="item-bg" src="#/img/portafolio/intermed-bg.png" alt="">
+                        <img class="img-left" src="#/img/portafolio/shadow-left.png" alt="">
+                        <img class="img-right" src="#/img/portafolio/shadow-right.png" alt="">
+                        <img class="logo" src="#/img/portafolio/intermed-logo.png" alt="">
+                        <div class="description">
+                            <p>Branding<span> / </span>Web<span> / </span>Impresos</p>
+                        </div>
+                    </div>
+                </div>
+                
 
 
 
