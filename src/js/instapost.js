4a33f5ac517fb8d73a9ac643a3b6953fd4861261
manuelchs <manuelchavarin@aori.mx>
2019-04-28 23:13:00 -0500

    insta post finished

--- src/js/instapost.js
+++ src/js/instapost.js
@@ -1,4 +1,5 @@
 var instaWidth = '100%';
+var animating = false;
 $( function() {
     for (let index = 0; index < $('.instapost-item').length; index++) {
         $('#references').append('<div class="reference"></div>');
@@ -10,46 +11,95 @@ $( function() {
     setReference();
 
     $('#instaLeft').click(function() {
-        instaLeft();
+        if ( !animating ) {
+            instaLeft();
+        }
     });
 
     $('#instaRight').click(function() {
-        instaRight();
+        if ( !animating ) {
+            instaRight();
+        }
+    });
+
+    var region = new ZingTouch.Region( document.getElementById('instapost'), true , false );
+    var target = document.getElementById('instapost');
+    region.bind(target, 'swipe', function(e) {
+        let angle = e.detail.data[0].currentDirection;
+        if ((angle >= 315 && angle <= 360) || (angle <= 45 && angle >= 0)) {
+            direction = 'right';
+        }
+        else if (angle >= 135 && angle <= 225) {
+            direction = 'left';
+        }
+        else if (angle <= 135) {
+            direction = 'up';
+            return;
+        } else {
+            direction = 'down';
+            return;
+        }
+        if ( direction === 'right' ) {
+            if ( window.screen.width <= breakPoint  ) {
+                if ( !animating ) {
+                    instaLeft();
+                }
+            }
+        } else if ( direction === 'left' ) {
+            if ( window.screen.width <= breakPoint  ) {
+                if ( !animating ) {
+                    instaRight();
+                }
+            }
+        }
     });
 });
 
 function instaRight() {
+    animating = true;
     var insta = document.getElementById('instapost');
     var marginToMove = ((Math.abs(parseInt(insta.style.marginLeft)) + 100) * -1) + '%';
-    $('#instapost').animate({ marginLeft: marginToMove }, 300, function() {
-        setButtons();
-        setReference();
-    });
+    console.log(parseInt(marginToMove));
+    if ( parseInt(marginToMove) > (instaWidth * -1) ) {
+        $('#instapost').animate({ marginLeft: marginToMove }, 300, function() {
+            animating = false;        
+            setButtons();
+            setReference();
+        });
+    } else {
+        animating = false;
+    }
 }
 
 function instaLeft() {
+    animating = true;
     var insta = document.getElementById('instapost');
     var marginToMove = ((Math.abs(parseInt(insta.style.marginLeft)) - 100) * -1) + '%';
-    $('#instapost').animate({ marginLeft: marginToMove }, 300, function() {
-        setButtons();
-        setReference();
-    });
+    console.log(parseInt(marginToMove));
+    if ( parseInt(marginToMove) <= 0) {
+        $('#instapost').animate({ marginLeft: marginToMove }, 300, function() {
+            animating = false;
+            setButtons();
+            setReference();
+        });
+    } else {
+        animating = false;
+    }
 }
 
 function setButtons() {
-    var insta = document.getElementById('instapost');
-    console.log(((instaWidth - 100) * -1 ) + '%');
-    console.log($('#instapost').css('margin-left'));
-    if ( $('#instapost').css('margin-left') == '0px' ) {
-        $('#instaLeft').css('display', 'none');
-        $('#instaRight').css('display', 'block');
-    } else if ( insta.style.marginLeft == ((instaWidth - 100) * -1 ) + '%' ) {
-        console.log('entro chido')
-        $('#instaLeft').css('display', 'block');
-        $('#instaRight').css('display', 'none');
-    } else {
-        $('#instaLeft').css('display', 'block');
-        $('#instaRight').css('display', 'block');
+    if ( screen.width > 768 ) {
+        var insta = document.getElementById('instapost');
+        if ( $('#instapost').css('margin-left') == '0px' ) {
+            $('#instaLeft').css('display', 'none');
+            $('#instaRight').css('display', 'block');
+        } else if ( insta.style.marginLeft == ((instaWidth - 100) * -1 ) + '%' ) {
+            $('#instaLeft').css('display', 'block');
+            $('#instaRight').css('display', 'none');
+        } else {
+            $('#instaLeft').css('display', 'block');
+            $('#instaRight').css('display', 'block');
+        }
     }
 }
 
